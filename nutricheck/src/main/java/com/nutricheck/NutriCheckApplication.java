ed3ce89ab11d97518357ad8b58d4d63e021f60c1
NayabJalal <jmd341602@gmail.com>
2026-01-05 23:32:26 +0530

    first commit

---
+++ nutricheck/src/main/java/com/nutricheck/NutriCheckApplication.java
@@ -0,0 +1,13 @@
+package com.nutricheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriCheckApplication.class, args);
+	}
+
+}
---
+++ nutricheck/src/test/java/com/nutricheck/NutriCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutricheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
